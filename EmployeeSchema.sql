73b7119fa18c242171867f75386730659b0e2323
Justin Marchi <marchij86@gmail.com>
2020-04-26 18:37:59 -0400

    Added schema with table creation

---
+++ EmployeeSchema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_managerDB;
+CREATE database employee_managerDB;
+
+USE employee_managerDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY id
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10,4),
+    department_id INT NOT NULL,
+    PRIMARY KEY id
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY id 
+)
